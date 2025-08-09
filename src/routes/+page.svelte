2a50c2bcd2987c1a3845c8639a8d2a7001a7dc56
bigtr3 <tmddms1002@gmail.com>
2025-08-10 00:11:02 +0900

    feat : 페이지 라우팅 설정

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<a href="/about">Go to about</a>
+
+<style>
+	p {
+		font-size: 2rem;
+	}
+</style>

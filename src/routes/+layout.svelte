7fec1338d7e69f05e119c19a9ad58a9a8b4a98cc
bigtr3 <tmddms1002@gmail.com>
2025-08-10 01:22:02 +0900

    feat : 화면 전환 애니메이션 적용

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
+	import { page } from '$app/stores';
 	import favicon from '$lib/assets/favicon.svg';
 	import { count } from '$lib/stores';
+	import { fade } from 'svelte/transition';
 </script>
 
 <svelte:head>
@@ -16,9 +18,12 @@
 
 <p>전체 클릭 수 : {$count}</p>
 
-<slot />
-
-<!--여기에 각 페이지 내용을 넣음-->
+<!-- key 블록으로 page.url.pathname 기준으로 페이지 전환 감지 -->
+{#key $page.url.pathname}
+	<div in:fade={{ delay: 150, duration: 300 }} out:fade={{ duration: 150 }}>
+		<slot />
+	</div>
+{/key}
 
 <!-- 네비게이션 바에만 적용-->
 <style>

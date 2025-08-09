23911e827b4eaf372ebad0fc9268b82b48671242
bigtr3 <tmddms1002@gmail.com>
2025-08-10 00:19:46 +0900

    feat : 레이아웃 설정

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,29 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
-
-	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<!-- 네비게이션 바 - 모든 페이지에서 보임 -->
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+</nav>
+
+<slot />
+
+<!--여기에 각 페이지 내용을 넣음-->
+
+<!-- 네비게이션 바에만 적용-->
+<style>
+	nav {
+		background: #eee;
+		padding: 1rem;
+	}
+	a {
+		margin-right: 1rem;
+		text-decoration: none;
+	}
+</style>
